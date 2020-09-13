8380f9ff36aa6728980a2b92aec79b8a8f1dc154
Saphire001 <spj29@njit.edu>
2020-09-13 15:22:18 -0400

    Add HW1 to Basics

---
+++ Basics/src/HW1.java
@@ -0,0 +1,19 @@
+
+public class HW1 
+{
+	public static void main(String[] args) 
+	{
+		String name = "Lilly"; 
+		
+		System.out.println("The name is: " + name);
+		
+		int age = 21;
+		
+		System.out.println("The age is: " + age);
+		
+		double var = 3.45;
+		
+		System.out.println("The variable is: " + var);
+	}
+
+}
