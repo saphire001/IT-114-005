30ba8d23a501643f94da0fa561cabff8964e3ddd
Saphire001 <spj29@njit.edu>
2020-09-13 14:08:44 -0400

    Adding changes

--- Basics/src/Hello.java
+++ Basics/src/Hello.java
@@ -1,6 +1,6 @@
-Public class HelloWorld
+public class Hello
 {
-  Public static void main(String[] args)
+  public static void main(String[] args)
   {
      System.out.println("Hello World!");
   }
