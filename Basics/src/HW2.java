097ede6f05b775a8dcb6a14ab52fc87502f30e37
Saphire001 <spj29@njit.edu>
2020-09-21 10:31:31 -0400

    hw 2 java loop

---
+++ Basics/src/HW2.java
@@ -0,0 +1,17 @@
+
+public class HW2 
+{
+	public static void main(String[] args) 
+	{
+		int[] a = new int[] {1,2,3,3,4,5,6};
+		
+		for(int n : a)
+		{
+			if(n % 2 == 0)
+			{
+				System.out.println(n);	
+			}
+		}
+	}
+
+}
